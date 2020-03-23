789a0fb6601ba7d7dd923512c5b45d04519ddf8f
William Chen <williamchen13225@gmail.com>
2020-03-23 16:17:11 -0700

    user functions and implement some socket functions on the server and client

--- server/index.js
+++ server/index.js
@@ -9,16 +9,34 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
+const {addUser, removeUser, getUser, getUserInRoom} = require('./users.js');
+
 io.on('connection', socket => {
-    console.log('user connected');
+   
+  socket.on('join', ({name, room}, callback) => {
+    const {error, user} = addUser({id:socket.id, name, room})
 
-    socket.on('join', ({name, room}, callback) => {
-        console.log(name, room);
-    })
+    if(error) return callback(error);
 
-    socket.on('disconnect', () => {
-        console.log('user disconnected');
-    })
+    socket.emit('message', {user: 'admin', text: `${user.name}, welcome to the room`})
+
+    socket.broadcast.to(user.room).emit('message', {user: 'admin', text: `${user.name} has joined`})
+
+    socket.join(user.room);
+
+    callback();
+  })
+
+  socket.on('sendMessage', (message, callback) => {
+    const user = getUser(socket.id);
+
+    io.to(user.room).emit('message', {user: user.name, text: message});
+    
+    callback();
+  })
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  })
 });
 
 app.use(router);
